798da1d6e0284ace37f0f34716aac546b6ec8b35
Declan Chidlow <git.rh92c@8shield.net>
2024-07-12 15:52:36 +0800

    Update script shebangs

--- mongo.sh
+++ mongo.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 mkdir -p ./private/mongo
 docker run -p 27017:27017 -v $(pwd)/private/mongo:/data/db mongo
--- redis.sh
+++ redis.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 docker run -p 6379:6379 eqalpha/keydb
