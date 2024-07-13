3111c35b986c2545f6db2014b93a8ac2acb4be94
Declan Chidlow <git.rh92c@8shield.net>
2024-07-13 19:14:20 +0800

    Update test command

--- bot/src/bot/commands/misc/test.ts
+++ bot/src/bot/commands/misc/test.ts
@@ -3,22 +3,22 @@ import SimpleCommand from "../../../struct/commands/SimpleCommand";
 import MessageCommandContext from "../../../struct/MessageCommandContext";
 
 export default {
-    name: 'test',
-    aliases: [ 'testalias' ],
-    description: 'Test command',
-    category: CommandCategory.Miscellaneous,
-    run: (message: MessageCommandContext, args: string[]) => {
-        message.reply({
-            content: 'Beep boop.',
-            embeds: [
-                {
-                    colour: "#ff0000",
-                    description: "embed description",
-                    title: "embed title",
-                    url: "https://amogus.org",
-                    icon_url: "https://amogus.org/amogus.png"
-                }
-            ],
-        });
-    }
+	name: "test",
+	aliases: ["testalias"],
+	description: "Test command",
+	category: CommandCategory.Miscellaneous,
+	run: (message: MessageCommandContext, args: string[]) => {
+		message.reply({
+			content: "Beep boop.",
+			embeds: [
+				{
+					colour: "#58A551",
+					title: "Test Success!",
+					description: "You've successfully tested the bot. It works.",
+					url: "https://automod.vale.rocks",
+					icon_url: "https://autumn.revolt.chat/avatars/pYjK-QyMv92hy8GUM-b4IK1DMzYILys9s114khzzKY",
+				},
+			],
+		});
+	},
 } as SimpleCommand;
