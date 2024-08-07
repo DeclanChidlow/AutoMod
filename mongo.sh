5ffe0a943481b7d9969ea46853d738a2f371a580
Declan Chidlow <git.rh92c@8shield.net>
2024-08-07 12:30:02 +0800

    Remove unneeded scripts

--- mongo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ./private/mongo
-docker run -p 27017:27017 -v $(pwd)/private/mongo:/data/db mongo
--- redis.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -p 6379:6379 eqalpha/keydb
