8e1944638d3c2908059d486728089d46504e359c
Declan Chidlow <git.rh92c@8shield.net>
2024-09-02 15:03:33 +0800

    Reintroduce debug/info command

---
+++ bot/src/bot/commands/miscellaneous/info.ts
@@ -0,0 +1,33 @@
+import SimpleCommand from "../../../struct/commands/SimpleCommand";
+import MessageCommandContext from "../../../struct/MessageCommandContext";
+import CommandCategory from "../../../struct/commands/CommandCategory";
+import { ULID_REGEX } from "../../util";
+import { decodeTime } from "ulid";
+
+const formatTimestamp = (timestamp: number): string => {
+	const timestampInSeconds = Math.round(timestamp / 1000);
+	return `Timestamp: \`${timestamp}\` (<t:${timestampInSeconds}:F> / <t:${timestampInSeconds}:R>)`;
+};
+
+const formatServerInfo = (context: MessageCommandContext): string => {
+	const serverId = context.channel?.serverId || "None";
+	return [`Server ID: \`${serverId}\``, `Server Context: \`${context.serverContext.id}\``, `Channel ID: \`${context.channelId}\``, `User ID: \`${context.authorId}\``].join("\n");
+};
+
+export default {
+	name: "info",
+	aliases: ["debug"],
+	description: "Provides info from a UUID or server.",
+	category: CommandCategory.Miscellaneous,
+	run: async (message: MessageCommandContext, args: string[]) => {
+		const [input] = args;
+
+		if (ULID_REGEX.test(input)) {
+			const timestamp = decodeTime(input);
+			const formattedTimestamp = formatTimestamp(timestamp);
+			await message.reply(`ULID: \`${input}\`\n${formattedTimestamp}`);
+		} else {
+			await message.reply(formatServerInfo(message));
+		}
+	},
+} as SimpleCommand;
