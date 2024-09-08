dedfd1474406831fe04a99b37ede86d9c37f2ddf
Declan Chidlow <git.rh92c@8shield.net>
2024-09-08 12:13:43 +0800

    Stop pinging everyone when using /stats (whoops)

--- bot/src/bot/commands/owner/stats.ts
+++ bot/src/bot/commands/owner/stats.ts
@@ -2,18 +2,19 @@ import SimpleCommand from "../../../struct/commands/SimpleCommand";
 import MessageCommandContext from "../../../struct/MessageCommandContext";
 import CommandCategory from "../../../struct/commands/CommandCategory";
 import { client } from "../../..";
-import child_process from "child_process";
 import { commands, ownerIDs } from "../../modules/command_handler";
 import fs from "fs";
 import path from "path";
 
-const getCommitHash = (): Promise<string | null> =>
-	new Promise((resolve) => {
-		child_process.exec("git rev-parse HEAD", (err, stdout) => {
-			if (err?.code) resolve(null);
-			else resolve(stdout);
-		});
-	});
+const pjson = JSON.parse((await fs.promises.readFile(path.join(process.cwd(), "package.json"))).toString());
+const now = Date.now();
+
+const formattedOwnerIDs = await Promise.all(
+	ownerIDs.map(async (id) => {
+		const user = await client.users.fetch(id);
+		return user ? `${user.username}#${user.discriminator} (\`${id}\`)` : `Unknown (\`${id}\`)`;
+	}),
+);
 
 export default {
 	name: "stats",
@@ -21,9 +22,6 @@ export default {
 	description: "Provides current AutoMod statistics.",
 	category: CommandCategory.Owner,
 	run: async (message: MessageCommandContext) => {
-		const pjson = JSON.parse((await fs.promises.readFile(path.join(process.cwd(), "package.json"))).toString());
-		const now = Date.now();
-		const formattedOwnerIDs = ownerIDs.map((id) => `<@${id}>`);
 		let msg =
 			`## AutoMod Stats\n` +
 			`### Cache\n` +
@@ -42,9 +40,7 @@ export default {
 			`### Miscellaneous\n` +
 			`Command count: \`${commands.length}\`\n` +
 			`Environment: \`${process.env["NODE_ENV"] || "testing"}\`\n` +
-			`Commit hash: \`${(await getCommitHash()) || "Unknown"}\`\n` +
-			`Owners: \`${formattedOwnerIDs.length}\` (${formattedOwnerIDs.join(", ")})\n`;
-
+			`Owners (${formattedOwnerIDs.length}):  ${formattedOwnerIDs.join(", ")}`;
 		await message.reply(msg, false);
 	},
 } as SimpleCommand;
