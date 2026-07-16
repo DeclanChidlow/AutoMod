7dc1a96f527cf05bd999e403edbfac82bbd448e5
Declan Chidlow <accounts@vale.rocks>
2026-07-16 14:05:47 +0800

    Add /spam list

--- packages/bot/src/bot/commands/configuration/spam.ts
+++ packages/bot/src/bot/commands/configuration/spam.ts
@@ -4,7 +4,15 @@ import MessageCommandContext from "../../../struct/MessageCommandContext";
 import { dbs } from "../../..";
 import { isBotManager, NO_MANAGER_MSG } from "../../util";
 
-const SYNTAX = "{prefix}spam enable; {prefix}spam disable";
+const SYNTAX = "{prefix}spam enable; {prefix}spam disable; {prefix}spam list";
+
+const MOD_ACTIONS: Record<number, string> = {
+	0: "Delete",
+	1: "Message",
+	2: "Warn",
+	3: "Kick",
+	4: "Ban",
+};
 
 export default {
 	name: "spam",
@@ -33,6 +41,28 @@ export default {
 
 				await dbs.SERVERS.updateOne({ id: message.serverContext.id }, { $set: { antispamEnabled: false } });
 				await message.reply("Spam detection is now **disabled** in this server.");
+				break;
+			}
+			case "list":
+			case "ls":
+			case "show": {
+				const rules = antispamEnabled?.automodSettings?.spam ?? [];
+				const dashboardUrl = process.env["WEB_UI_URL"] || "https://automod.vale.rocks";
+				if (rules.length === 0) {
+					await message.reply(`No spam rules configured.\n\n[Open Dashboard](${dashboardUrl}/dashboard/${message.serverContext.id}/antispam) to create one.`);
+					break;
+				}
+				let output = "## Anti-Spam Rules\n";
+				output += `| Trigger | Action | Message |\n`;
+				output += `|---------|--------|--------|\n`;
+				for (const rule of rules) {
+					const trigger = `${rule.max_msg} messages in ${rule.timeframe} second${rule.timeframe !== 1 ? "s" : ""}`;
+					const action = MOD_ACTIONS[rule.action] ?? `Unknown (${rule.action})`;
+					const msg = rule.message ? rule.message.replace(/\|/g, "\\|") : "N/A";
+					output += `| ${trigger} | ${action} | ${msg} |\n`;
+				}
+				output += `\n[Open the web dashboard](${dashboardUrl}/dashboard/${message.serverContext.id}/antispam) to manage rules.`;
+				await message.reply(output);
 				break;
 			}
 			default: {
